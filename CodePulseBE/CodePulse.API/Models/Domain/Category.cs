e1e2836ef2631ce995bfdda863768e0dd4d67308
MurariuF <murariu.flavius@gmail.com>
2024-03-26 14:45:45 +0200

    first push

---
+++ CodePulseBE/CodePulse.API/Models/Domain/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public bool IsVisible { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
