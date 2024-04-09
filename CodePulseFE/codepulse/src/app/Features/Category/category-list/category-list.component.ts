d0cdd3bef724b43359e5db4123ed02a3d916a8e6
MurariuF <murariu.flavius@gmail.com>
2024-04-09 14:06:42 +0300

    Pagination added

--- CodePulseFE/codepulse/src/app/Features/Category/category-list/category-list.component.ts
+++ CodePulseFE/codepulse/src/app/Features/Category/category-list/category-list.component.ts
@@ -11,11 +11,28 @@ import { Observable } from 'rxjs';
 export class CategoryListComponent implements OnInit {
 
   categories$?: Observable<Category[]>
+  totalCount?: number;
+  pageNumber = 1;
+  pageSize = 5;
+  list: number[] = [];
 
   constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
-    this.categories$ = this.categoryService.getAllCategories();
+    this.categoryService.getCategoryCount()
+      .subscribe({
+        next: (value) => {
+          this.totalCount = value;
+          this.list = new Array(Math.ceil(value / this.pageSize))
+          this.categories$ = this.categoryService.getAllCategories(
+            undefined,
+            undefined,
+            undefined,
+            this.pageNumber,
+            this.pageSize
+          );
+        }
+      })
   }
 
   onSearch(query: string) {
@@ -24,5 +41,45 @@ export class CategoryListComponent implements OnInit {
 
   sort(sortBy: string, sortDirection: string) {
     this.categories$ = this.categoryService.getAllCategories(undefined, sortBy, sortDirection);
+  }
+
+  getPage(pageNumber: number) {
+    this.pageNumber = pageNumber;
+
+    this.categories$ = this.categoryService.getAllCategories(
+      undefined,
+      undefined,
+      undefined,
+      this.pageNumber,
+      this.pageSize
+    );
+  }
+
+  getPreviousPage() {
+    if (this.pageNumber - 1 < 1) {
+      return ;
+    }
+    this.pageNumber --;
+    this.categories$ = this.categoryService.getAllCategories(
+      undefined,
+      undefined,
+      undefined,
+      this.pageNumber,
+      this.pageSize
+    );
+  }
+  
+  getNextPage() {
+    if (this.pageNumber + 1 > this.list.length) {
+      return ;
+    }
+    this.pageNumber ++;
+    this.categories$ = this.categoryService.getAllCategories(
+      undefined,
+      undefined,
+      undefined,
+      this.pageNumber,
+      this.pageSize
+    );
   }
 }
